b912fea2ec07c0d603af1fa73eb0c71acda299e3
Russell Welch <russellwelch17@gmail.com>
2023-09-12 12:26:42 -0400

    Added comment rules

--- functions.h
+++ functions.h
@@ -3,3 +3,14 @@
  *CPSC 2311 001
  *rowelch@clemson.edu
  *************************/
+
+
+// All functions need the following:
+/* Parameters: img - image_t pointer array holding the image data for
+ *                     each of the input files
+ * Return:        output - image_t struct containing output image data
+ * This function averages every pixels rbg values from each of the 
+ * input images and puts those averages into a single output image
+ */
+
+// All comments inside a function must be above the lines of code, not inline
